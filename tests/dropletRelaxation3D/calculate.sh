c459d05234195238b16ff95aeb6e026edf59564e
Alex Gubkin <alexshtil@gmail.com>
2022-11-07 10:30:04 +0500

    FSF solver test

--- tests/dropletRelaxation3D/calculate.sh
+++ tests/dropletRelaxation3D/calculate.sh
@@ -15,8 +15,8 @@ cp -r\
 
 #Preparation mesh
 foamDictionary  -entry decomposer -set scotch system/decomposeParDict
-foamDictionary  -entry numberOfSubdomains -set 60 system/decomposeParDict
-# foamDictionary  -entry simpleCoeffs/n -set "(3 3 3)" system/decomposeParDict
+foamDictionary  -entry numberOfSubdomains -set 6 system/decomposeParDict
+# foamDictionary  -entry simpleCoeffs/n -set "(1 2 3)" system/decomposeParDict
 runApplication  blockMesh
 runApplication  decomposePar
 runParallel     renumberMesh -overwrite
@@ -30,11 +30,14 @@ rm log.transformPoints
 runParallel     transformPoints "scale=(${scaleSize} ${scaleSize} ${scaleSize})"
 
 foamDictionary  -entry endTime -set $(echo "0.005" | bc -l) system/controlDict
-foamDictionary  -entry deltaT -set $(echo "0.000000001" | bc -l) system/controlDict
+foamDictionary  -entry deltaT -set $(echo "0.000001" | bc -l) system/controlDict
+foamDictionary  -entry writeControl -set "adjustableRunTime" system/controlDict
+# foamDictionary  -entry writeControl -set "timeStep" system/controlDict
 foamDictionary  -entry writeInterval -set $(echo "0.00001" | bc -l) system/controlDict
+# foamDictionary  -entry writeInterval -set 1 system/controlDict
 foamDictionary  -entry adjustTimeStep -set "yes" system/controlDict
-foamDictionary  -entry maxCo -set $(echo "0.1" | bc -l) system/controlDict
-foamDictionary  -entry maxAlphaCo -set $(echo "0.1" | bc -l) system/controlDict
+foamDictionary  -entry maxCo -set $(echo "0.2" | bc -l) system/controlDict
+foamDictionary  -entry maxAlphaCo -set $(echo "0.2" | bc -l) system/controlDict
 
 # runParallel     interFoam
 # runParallel     interSSFFoam
