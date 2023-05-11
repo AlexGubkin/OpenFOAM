966e0008e41cc2690d362912a42112e118170138
Alex <alexshtil@gmail.com>
2023-05-11 17:55:22 +0500

    Hele-Shaw solver should be tested

---
+++ tests/HeleShaw/channel2D/calculate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+#Variables section
+caseName=case1
+
+scaleSize=$(echo "0.000001" | bc -l)
+
+cd ${caseName}
+
+cp -r\
+    0.orig\
+    0
+
+#Preparation mesh
+foamDictionary  -entry decomposer -set scotch system/decomposeParDict
+foamDictionary  -entry numberOfSubdomains -set 6 system/decomposeParDict
+# foamDictionary  -entry simpleCoeffs/n -set "(1 2 3)" system/decomposeParDict
+runApplication  blockMesh
+runApplication  decomposePar
+runParallel     renumberMesh -overwrite
+runParallel     checkMesh -allGeometry -allTopology
+
+#Calculation
+runApplication  setFields
+runParallel -a  setFields
+runApplication  transformPoints "scale=(${scaleSize} ${scaleSize} ${scaleSize})"
+runParallel -a  transformPoints "scale=(${scaleSize} ${scaleSize} ${scaleSize})"
+
+foamDictionary  -entry endTime -set $(echo "10.0" | bc -l) system/controlDict
+foamDictionary  -entry deltaT -set $(echo "0.00000001" | bc -l) system/controlDict
+foamDictionary  -entry writeControl -set "adjustableRunTime" system/controlDict
+# foamDictionary  -entry writeControl -set "timeStep" system/controlDict
+foamDictionary  -entry writeInterval -set $(echo "0.001" | bc -l) system/controlDict
+# foamDictionary  -entry writeInterval -set 1 system/controlDict
+foamDictionary  -entry adjustTimeStep -set "yes" system/controlDict
+foamDictionary  -entry maxCo -set $(echo "0.2" | bc -l) system/controlDict
+foamDictionary  -entry maxAlphaCo -set $(echo "0.2" | bc -l) system/controlDict
+
+runParallel     interHeleShawFoam
+
+
+exit 0
---
+++ tests/HeleShaw/channel2D/createCase.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Variables section
+caseName=case1
+
+function makeCase(){
+    mkdir -p ${caseName}
+
+    cp -r src/0.orig    ${caseName}
+    cp -r src/constant  ${caseName}
+    cp -r src/system    ${caseName}
+}
+
+makeCase
+
+echo -e "\nCreation done!\n"
+
+exit 0
